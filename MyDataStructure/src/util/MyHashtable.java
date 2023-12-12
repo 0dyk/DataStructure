944f3f2affc282cc7d966600602cd242826c50a0
yeongdae <oooo_0@naver.com>
2023-12-12 18:32:54 +0900

    12/12

--- MyDataStructure/src/util/MyHashtable.java
+++ MyDataStructure/src/util/MyHashtable.java
@@ -2,9 +2,9 @@ package util;
 
 import java.util.*;
 
-public class MyHashtable<K,V> implements MyMap<K, V> {
+public class MyHashtable<K,V> implements MyMap<K, V>, java.io.Serializable {
 
-    private transient Entry<?,?>[] table;
+    private static final long serialVersionUID = 1421746759512286392L;
 
     private static class Entry<K,V> implements Map.Entry<K,V> {
         final int hash;
@@ -62,60 +62,256 @@ public class MyHashtable<K,V> implements MyMap<K, V> {
         }
     }
 
+    private transient Entry<?,?>[] table;
 
+    private transient int count;
 
-    @Override
-    public int size() {
-        return 0;
+    // 테이블 크기 임계값
+    private int threshold;
+
+    private float loadFactor;
+
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
+    public MyHashtable() {
+        this(11, 0.75f);
+    }
+
+    public MyHashtable(int initialCapacity) {
+        this(initialCapacity, 0.75f);
+    }
+
+    public MyHashtable(int initialCapacity, float loadFactor) {
+        if (initialCapacity < 0)
+            throw new IllegalArgumentException("Illegal Capacity: "+
+                    initialCapacity);
+        if (loadFactor <= 0 || Float.isNaN(loadFactor))
+            throw new IllegalArgumentException("Illegal Load: "+loadFactor);
+
+        if (initialCapacity==0)
+            initialCapacity = 1;
+        this.loadFactor = loadFactor;
+        table = new Entry<?,?>[initialCapacity];
+        threshold = (int)Math.min(initialCapacity * loadFactor, MAX_ARRAY_SIZE + 1);
     }
 
     @Override
-    public boolean isEmpty() {
+    public synchronized int size() {
+        return count;
+    }
+
+    @Override
+    public synchronized boolean isEmpty() {
+        return count == 0;
+    }
+
+    // Enumeration
+
+    public synchronized boolean contains(Object value) {
+        if (value == null) {
+            throw new NullPointerException();
+        }
+
+        Entry<?,?> tab[] = table;
+        for (int i = tab.length ; i-- > 0 ;) {
+            for (Entry<?,?> e = tab[i]; e != null ; e = e.next) {
+                if (e.value.equals(value)) {
+                    return true;
+                }
+            }
+        }
         return false;
     }
 
     @Override
-    public boolean containsKey(Object key) {
+    public synchronized boolean containsKey(Object key) {
+        Entry<?,?> tab[] = table;
+        int hash = key.hashCode();
+        int index = (hash & 0x7FFFFFFF) % tab.length;
+        for (Entry<?,?> e = tab[index]; e != null ; e = e.next) {
+            if ((e.hash == hash) && e.key.equals(key)) {
+                return true;
+            }
+        }
         return false;
     }
 
     @Override
     public boolean containsValue(Object value) {
-        return false;
+        return contains(value);
     }
 
     @Override
-    public V get(Object key) {
+    public synchronized V get(Object key) {
+        Entry<?,?> tab[] = table;
+        int hash = key.hashCode();
+        int index = (hash & 0x7FFFFFFF) % tab.length;
+        for (Entry<?,?> e = tab[index]; e != null ; e = e.next) {
+            if ((e.hash == hash) && e.key.equals(key)) {
+                return (V)e.value;
+            }
+        }
+        return null;
+    }
+
+    private void addEntry(int hash, K key, V value, int index) {
+//        modCount++;
+
+        Entry<?,?> tab[] = table;
+        if (count >= threshold) {
+            // Rehash the table if the threshold is exceeded
+            rehash();
+
+            tab = table;
+            hash = key.hashCode();
+            index = (hash & 0x7FFFFFFF) % tab.length;
+        }
+
+        // Creates the new entry.
+        @SuppressWarnings("unchecked")
+        Entry<K,V> e = (Entry<K,V>) tab[index];
+        tab[index] = new Entry<>(hash, key, value, e);
+        count++;
+    }
+
+    @Override
+    public synchronized V put(K key, V value) {
+        // Make sure the value is not null
+        if (value == null) {
+            throw new NullPointerException();
+        }
+
+        // Makes sure the key is not already in the hashtable.
+        Entry<?,?> tab[] = table;
+        int hash = key.hashCode();
+        int index = (hash & 0x7FFFFFFF) % tab.length;
+        @SuppressWarnings("unchecked")
+        Entry<K,V> entry = (Entry<K,V>)tab[index];
+        for(; entry != null ; entry = entry.next) {
+            if ((entry.hash == hash) && entry.key.equals(key)) {
+                V old = entry.value;
+                entry.value = value;
+                return old;
+            }
+        }
+
+        addEntry(hash, key, value, index);
         return null;
     }
 
     @Override
-    public V put(K key, V value) {
+    public synchronized V remove(Object key) {
+        Entry<?,?> tab[] = table;
+        int hash = key.hashCode();
+        int index = (hash & 0x7FFFFFFF) % tab.length;
+        @SuppressWarnings("unchecked")
+        Entry<K,V> e = (Entry<K,V>)tab[index];
+        for(Entry<K,V> prev = null; e != null ; prev = e, e = e.next) {
+            if ((e.hash == hash) && e.key.equals(key)) {
+//                modCount++;
+                if (prev != null) {
+                    prev.next = e.next;
+                } else {
+                    tab[index] = e.next;
+                }
+                count--;
+                V oldValue = e.value;
+                e.value = null;
+                return oldValue;
+            }
+        }
         return null;
     }
 
     @Override
-    public V remove(Object key) {
-        return null;
+    public synchronized void clear() {
+        Entry<?,?> tab[] = table;
+//        modCount++;
+        for (int index = tab.length; --index >= 0; )
+            tab[index] = null;
+        count = 0;
     }
 
-    @Override
-    public void clear() {
-
-    }
+    private transient volatile Set<K> keySet;
+    private transient volatile Set<Map.Entry<K,V>> entrySet;
+    private transient volatile Collection<V> values;
 
     @Override
     public Set<K> keySet() {
-        return null;
+        if (keySet == null)
+            keySet = Collections.synchronizedSet(new MyHashtable.KeySet(), this);
+        return keySet;
     }
+
+    private class KeySet extends AbstractSet<K> {
+        public Iterator<K> iterator() {
+            return getIterator(KEYS);
+        }
+        public int size() {
+            return count;
+        }
+        public boolean contains(Object o) {
+            return containsKey(o);
+        }
+        public boolean remove(Object o) {
+            return MyHashtable.this.remove(o) != null;
+        }
+        public void clear() {
+            MyHashtable.this.clear();
+        }
+    }
+
+    @Override
+    public Set<Map.Entry<K,V>> entrySet() {
+        if (entrySet==null)
+            entrySet = Collections.synchronizedSet(new MyHashtable.EntrySet(), this);
+        return entrySet;
+    }
+
 
     @Override
     public Collection<V> values() {
         return null;
     }
 
-    @Override
-    public Set<Map.Entry<K, V>> entrySet() {
-        return null;
+
+
+
+
+
+
+
+
+
+    // 오...
+    @SuppressWarnings("unchecked")
+    protected void rehash() {
+        int oldCapacity = table.length;
+        Entry<?,?>[] oldMap = table;
+
+        // overflow-conscious code
+        int newCapacity = (oldCapacity << 1) + 1;
+        if (newCapacity - MAX_ARRAY_SIZE > 0) {
+            if (oldCapacity == MAX_ARRAY_SIZE)
+                // Keep running with MAX_ARRAY_SIZE buckets
+                return;
+            newCapacity = MAX_ARRAY_SIZE;
+        }
+        Entry<?,?>[] newMap = new Entry<?,?>[newCapacity];
+
+//        modCount++;
+        threshold = (int)Math.min(newCapacity * loadFactor, MAX_ARRAY_SIZE + 1);
+        table = newMap;
+
+        for (int i = oldCapacity ; i-- > 0 ;) {
+            for (Entry<K,V> old = (Entry<K,V>)oldMap[i]; old != null ; ) {
+                Entry<K,V> e = old;
+                old = old.next;
+
+                int index = (e.hash & 0x7FFFFFFF) % newCapacity;
+                e.next = (Entry<K,V>)newMap[index];
+                newMap[index] = e;
+            }
+        }
     }
 }
